50ac2d883b44174f4f8ba3ae1bb7dbba9f368ea6
earloc <earloc@live.de>
2017-05-29 14:55:29 +0200

    added netstandard1.0

--- Blue.MVVM.Commands_/Properties/_AssemblyInfo.cs
+++ Blue.MVVM.Commands_/Properties/_AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
+#if !NETSTANDARD1_0
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Blue.MVVM")]
-[assembly: AssemblyCopyright("Copyright © nobody in particular 2014 - 2016")]
+[assembly: AssemblyCopyright("Copyright © nobody in particular 2014 - 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -28,5 +28,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.1.0")]
-[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
+
+#endif
